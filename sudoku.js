c7a35f879df0c82d4af6a02ef5686eb4ae132dfd
Alexandre <alexandre707@live.com>
2019-03-24 18:05:46 -0400

    cleaned up the code a bit

--- sudoku.js
+++ sudoku.js
@@ -4,12 +4,9 @@ fillTable();
 
 function genTable() {
 
-    var html;
+    var html = "<table>";
 
-    html = "<table>";
-
-    var x;
-    for(x = 0; x < 9; x++){
+    for(var x = 0; x < 9; x++){
         html = html + "<tr>";
         var y;
         for(y = 0; y < 9; y++){
@@ -42,7 +39,6 @@ function scany(x) {
 
     for(i = 0; i < 9; i++){
         values[i] = document.getElementById(x + "" + i).innerHTML;
-        //console.log(values[i]);Maple Test 3
     }
     return values;
 }
@@ -51,21 +47,16 @@ function scanbox(x, y) {
 
     var values = [];
     var count = 0;
-    console.log("Square x:" + x + " " + "y:" + y);
 
     //Gets which 3x3 box its in
     x = Math.floor(x/3);
     y = Math.floor(y/3);
-    console.log("Box pos x:" + x + " y:" + y);
-    console.log("xmin:" + (x*3) + " ymin:" + (y*3));
-    console.log("xmax:" + (x*3 + 2) + " Ymax:" + (y*3+2));
 
     for(var j = (y*3); j <= (y*3 + 2); j++){
 
         for(var i = (x*3); i <= (x*3 + 2); i++){
             values[count] = document.getElementById(i + "" + j).innerHTML;
             count++;
-            console.log(values[count-1]);
         }
     }
     return values;
@@ -84,19 +75,13 @@ function possibleval(x, y) {
         values_possible [i] = i + 1;
     }
 
-
     removecommon(values_possible, values_x);
     removecommon(values_possible, values_y);
-    console.log(values_xy);
     removecommon(values_possible, values_xy);
-
-
-
 
     if(values_possible.length === 0){
         values_possible[0] = 0;
     }
-
 
     return values_possible;
 }
@@ -108,12 +93,9 @@ function scan0(){
             if(document.getElementById(x+ "" + y).innerHTML == 0){
                 return true;
             }
-
         }
     }
-
     return false;
-
 }
 
 function ranInt(max) {
@@ -122,16 +104,11 @@ function ranInt(max) {
 
 function removecommon(array1, temp){
 
-
     var array2 = [];
-    var values_possible = [];
     for(var i = 0; i < temp.length; i++){
         array2[i] = parseInt(temp[i]);
     }
 
-
-    //var array1 = ['1', '2', '3', '4', '5'];
-    //var array2 = ['4', '5'];
     var index;
     for (var i=0; i<array2.length; i++) {
         index = array1.indexOf(array2[i]);
@@ -139,7 +116,6 @@ function removecommon(array1, temp){
             array1.splice(index, 1);
         }
     }
-
     return array1;
 }
 
@@ -150,19 +126,13 @@ function fillTable() {
             var setvalue = possibleval(x, y);
             document.getElementById(x+ "" + y).innerHTML = setvalue[ranInt(setvalue.length)];
 
+            //Comment out the if block below to prevent brute forcing of puzzle
             /*
-            if(document.getElementById(8+ "" + 8).innerHTML != 0){
-                console.log("sudoku puzzle generated sucessfully");
-            } else {
-                var values = possibleval(x, y);
-                if(values[0] != 0){
-                    fillTable()
-                } else {
-
-                }
+            if(setvalue[0] === 0){
+                y = 0;
+                x = 0;
             }
             */
-
         }
     }
 }
@@ -175,6 +145,5 @@ function storeToArray(){
             values = parseInt(document.getElementById(x+ "" + y).innerHTML);
         }
     }
-    console.log(values);
     return values;
 }
